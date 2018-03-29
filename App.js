324f89a0f57b6204866d927e0c3f9eb525152a4f
Ryan Donnelly <ryanjdonnelly@gmail.com>
2018-03-29 17:16:07 -0400

    update text and add button

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.text}>Hey Detroit Labs!</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Tap Me</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -16,8 +17,23 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fcfcfc',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'gray',
+    fontSize: 42,
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: 'blue',
+    borderRadius: 12,
+    paddingHorizontal: 36,
+    paddingVertical: 12,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 24,
   },
 });
